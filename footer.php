5fdfe1b36adb7c7d1885d4d75644479ee4c86bc6
pedrOfsilveira <pedrofsilveira24@gmail.com>
2024-09-19 22:07:36 -0300

    Revert "teste"
    This reverts commit 1ac5ff30a9e3af60a02c13d17d0fb56bb190e63f.

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  <div class="footer_centro"><a href="index.php"><img class="rodalogo" src="img/logo.png" alt="logo rodapé"></a>
-    <div class="textoroda"><a href="#">Início</a> <a href="sobre.php">Sobre nós</a> <br></div>
-    <p class="copy">© Wikiposa</p>
-  </div>
-</footer>
-
